53e539e08ba960326936ea68df8c31a92218ed65
ernestgwilsonii <ernestgwilsonii@gmail.com>
2017-11-10 10:12:09 -0500

    added organization security to restrict the bot to the current orgId

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ const accessToken = process.env.SPARK_TOKEN;
 const PORT = process.env.PORT || 3000;
 const { chatOpsLogger } = require('./lib/chatOpsLogger.js');
 const { ciscoSparkGetPersonDetails } = require('./lib/ciscoSparkGetPersonDetails.js');
+const { ciscoSparkVerifyOrgSecurity } = require('./lib/ciscoSparkVerifyOrgSecurity.js');
 const moment = require('moment-timezone');
 const sparkWebSocket = require('ciscospark-websocket-events');
 const webHookUrl = "http://localhost:" + PORT + "/ciscospark/receive";
@@ -50,9 +51,13 @@ controller.setupWebserver(PORT, function (err, webserver) {
 ////////////////////////////////////////////////////////////////////////////////
 // hello //
 ///////////
-controller.hears(['hello', 'greetings', 'hi'], 'direct_message,direct_mention', function (bot, message) {
-    const { chatOps_hello } = require('./chatOps_hello.js');
-    chatOps_hello(bot, message);
+controller.hears(['hello', 'greetings'], 'direct_message,direct_mention', function (bot, message) {
+    ciscoSparkVerifyOrgSecurity(bot, message, function (error) {
+        if (!error) {
+            const { chatOps_hello } = require('./chatOps_hello.js');
+            chatOps_hello(bot, message);
+        }
+    })
 });
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -61,8 +66,12 @@ controller.hears(['hello', 'greetings', 'hi'], 'direct_message,direct_mention', 
 // ping TARGET //
 /////////////////
 controller.hears('ping', 'direct_message,direct_mention', function (bot, message) {
-    const { chatOps_ping } = require('./chatOps_ping.js');
-    chatOps_ping(bot, message);
+    ciscoSparkVerifyOrgSecurity(bot, message, function (error) {
+        if (!error) {
+            const { chatOps_ping } = require('./chatOps_ping.js');
+            chatOps_ping(bot, message);
+        }
+    })
 });
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -71,8 +80,12 @@ controller.hears('ping', 'direct_message,direct_mention', function (bot, message
 // zipcode lookuup //
 /////////////////////
 controller.hears(['zipcode lookup', 'zip lookup'], 'direct_message,direct_mention', function (bot, message) {
-    const { chatOps_zipCode_LookUp } = require('./chatOps_zipCode_LookUp.js');
-    chatOps_zipCode_LookUp(bot, message);
+    ciscoSparkVerifyOrgSecurity(bot, message, function (error) {
+        if (!error) {
+            const { chatOps_zipCode_LookUp } = require('./chatOps_zipCode_LookUp.js');
+            chatOps_zipCode_LookUp(bot, message);
+        }
+    })
 });
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -82,35 +95,42 @@ controller.hears(['zipcode lookup', 'zip lookup'], 'direct_message,direct_mentio
 //////////////////////
 // Catch "all" HELP function  - For "help" or anything unknown sent to the bot!
 controller.on('direct_message,direct_mention', function (bot, message) {
-    const { sparkBotHelpDirectMention, sparkBotHelpDirectMessage } = require('./botHelp.js');
-    const allArguments = message.text.replace(/\s+/g, " ");
-    const Arg = allArguments.split(" ");
-    let sparkMessage = [];
-    message.command = "help"; // <--Set this value for acurate logging and reporting 
-    ciscoSparkGetPersonDetails(message, function (error, person) {
-        if (error) {
-            message.logLevel = 'ERROR';
-            let thisReply = "Bummer... \n\n" + error;
-            sparkMessage.push(thisReply);
-            bot.reply(message, sparkMessage.join(''));
-        } else if (message.raw_message.data.roomType == 'group') {
-            if (Arg[0].toUpperCase() !== "HELP") {
-                message.logLevel = 'WARNING';
-            }
-            let thisReply = "Hello " + person.firstName + "!\n\n";
-            sparkMessage.push(thisReply);
-            sparkMessage.push(sparkBotHelpDirectMention); // Send help message formatted for a group space
-            bot.reply(message, sparkMessage.join(''));
-        } else if (message.raw_message.data.roomType == 'direct') {
-            if (Arg[0].toUpperCase() !== "HELP") {
-                message.logLevel = 'WARNING';
-            }
-            let thisReply = "Hello " + person.firstName + "!\n\n";
-            sparkMessage.push(thisReply);
-            sparkMessage.push(sparkBotHelpDirectMessage); // Send help message formatted for a private message
-            bot.reply(message, sparkMessage.join(''));
+
+    ciscoSparkVerifyOrgSecurity(bot, message, function (error) {
+        if (!error) {
+
+            const { sparkBotHelpDirectMention, sparkBotHelpDirectMessage } = require('./botHelp.js');
+            const allArguments = message.text.replace(/\s+/g, " ");
+            const Arg = allArguments.split(" ");
+            let sparkMessage = [];
+            message.command = "help"; // <--Set this value for acurate logging and reporting 
+            ciscoSparkGetPersonDetails(message, function (error, person) {
+                if (error) {
+                    message.logLevel = 'ERROR';
+                    let thisReply = "Bummer... \n\n" + error;
+                    sparkMessage.push(thisReply);
+                    bot.reply(message, sparkMessage.join(''));
+                } else if (message.raw_message.data.roomType == 'group') {
+                    if (Arg[0].toUpperCase() !== "HELP") {
+                        message.logLevel = 'WARNING';
+                    }
+                    let thisReply = "Hello " + person.firstName + "!\n\n";
+                    sparkMessage.push(thisReply);
+                    sparkMessage.push(sparkBotHelpDirectMention); // Send help message formatted for a group space
+                    bot.reply(message, sparkMessage.join(''));
+                } else if (message.raw_message.data.roomType == 'direct') {
+                    if (Arg[0].toUpperCase() !== "HELP") {
+                        message.logLevel = 'WARNING';
+                    }
+                    let thisReply = "Hello " + person.firstName + "!\n\n";
+                    sparkMessage.push(thisReply);
+                    sparkMessage.push(sparkBotHelpDirectMessage); // Send help message formatted for a private message
+                    bot.reply(message, sparkMessage.join(''));
+                }
+                chatOpsLogger(message, sparkMessage.join(''));
+            });
+
         }
-        chatOpsLogger(message, sparkMessage.join(''));
-    });
+    })
 });
 ////////////////////////////////////////////////////////////////////////////////
